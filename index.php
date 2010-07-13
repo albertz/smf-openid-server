73e92903c2cb1e6c9a1deceb34965186e064903c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-07-13 18:45:37 +0200

    always multi user

--- index.php
+++ index.php
@@ -16,21 +16,7 @@ if ($_SESSION['auth_username']) {
 }
 
 if (!$included) {
-	
-	// Check to see how many config files exist
-	$config_files = glob('./config/*.php');
-
-	// If there aren't any config files, then redirect to install script
-	if (!$config_files) {
-		header('location: ./install.php');
-		exit;
-	}
-
-	if (count($config_files) > 1) {
-		require('./multi_user.php'); // load multi user config
-	} else {
-		require($config_files[0]); // load single user config
-	}
+	require('./multi_user.php'); // load multi user config
 }
 
 require_once ('./phpmyid.php');
